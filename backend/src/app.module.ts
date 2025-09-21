b1b5b4230eb5e83a823c0c7001175ab5a91dcc1b
SamBeak <baekhg102@korea.ac.kr>
2025-09-21 20:21:13 +0900

    chore: add TypeORM and config dependencies for database setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+	TypeOrmModule.forRootAsync({
+		imports: [ConfigModule],
+		inject: [ConfigService],
+		useFactory: (configService: ConfigService) => ({
+			type: 'postgres',
+			host: configService.get('DB_HOST'),
+			port: configService.get('DB_PORT'),
+			username: configService.get('DB_USERNAME'),
+			password: configService.get('DB_PASSWORD'),
+			database: configService.get('DB_DATABASE'),
+			synchronize: configService.get('DB_SYNCHRONIZE'),
+			logging: configService.get('DB_LOGGING'),
+		}),
+	}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
