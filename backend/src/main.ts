15a95aaf326f6e29f4bfb99914ca16ce8481f3a2
SamBeak <baekhg102@korea.ac.kr>
2025-10-13 20:44:18 +0900

    feat: add Swagger API
     add Swagger API documentation setup with OpenAPI config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('API Document')
+    .setDescription('API Document for This Project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
